973ba7b3a2c6fc1628fc712c11efbe3fa38064bf
ForestLang <150213844+ForestLang@users.noreply.github.com>
2023-12-01 11:32:02 -0500

    update to main.cpp
    mostly finished, works for provided data set

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ int main() {
 			needs[p][r] = totalNeeded[p][r] - have[p][r];
 		}
 	}
+
+	/* uncomment to see which values are being entered into needs
 	// print the values of the needs table to check everything is entered correctly
 	for(int p = 0; p < n; p++) {
 		for(int r = 0; r < m; r++) {
@@ -21,29 +23,57 @@ int main() {
 		}
 		cout << "\n";
 	}
+	*/
+
 	// create an array for the safe order to be saved in
 	int safe[n];
 	int pnum = 0; // a int for keeping track of which process is being looked at
-	int numMisses = 0; // int for making sure I maybe don't get stuck in an endless loop
 	// start loop for entering things into safe[]
 	for(int s = 0; s < n; s++) {
-		bool safeP = false;
 		//start loop that ckecks if it is safe to allocate the resources to process pnum
-			// if it is safe, safeP = true
-			// else: do nothing
-		if(safeP)
+		int safeAlloc = 0;
+		for(int r = 0; r < m; r++) {
+			if(avalible[r] + have[pnum][r] >= totalNeeded[pnum][r])
+				safeAlloc++;
+			else {  // exits loop and set safeAlloc to a low value
+				r = m + 1;
+				safeAlloc = -1;
+			}
+		}
+		// if it is safe, safeAlloc == m
+		if(safeAlloc == m) {
 			safe[s] = pnum;
+			// vVv uncomment to see which values are being entered into safe[] vVv
+			// cout << "safe[" << s << "] == P" << safe[s] << "\n";
+			// increase avalible[] values by have[pnum][]
+			for(int i = 0; i < m; i++) {
+				avalible[i] += have[pnum][i];
+			}
+		}
 		else {
 			s--;
-			numMisses++;
 		}
-		if(numMisses == n) {
-			if(s == 0) {
-				cout << "cannot find safe sequence with provided variables";
-				s == n+3; //exits loop
+
+		// increase pnum
+		pnum++;
+		if(pnum == n)
+			pnum = 0;
+		// check it the new value of pnum is in safe[]
+		for(int i = 0; i <= 0; i++) {
+			while(pnum == safe[i]) {
+				// if it is. increment it again
+				pnum++;
+				if(pnum == n)
+					pnum = 0;
 			}
-			else
-				s--;
 		}
 	}
+
+	// print values of safe
+	cout << "the provided data is a safe system\n";
+	cout << "the safe sequence is P" << safe[0];
+	for(int i = 1; i < n; i++) {
+		cout << ", P" << safe[i];
+	}
+	cout << "!\n";
 }
